070f9a02dbc6565e61234015d1c809db74d403ae
dashakoryukova <dashakoryukova@gmail.com>
2014-04-11 13:45:19 +0400

    work project

---
+++ Test/Test/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Test.Startup))]
+
+namespace Test
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Дополнительные сведения о настройке приложения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login")
+            });
+        
+        }
+    }
+}
